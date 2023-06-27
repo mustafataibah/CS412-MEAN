8cb272289625bda45572083e862d2ca5e84058ed
Mustafa <mustafataibah@gmail.com>
2023-06-27 06:21:22 -0400

    updated server.js to use redis

--- PS5/Node/server.js
+++ PS5/Node/server.js
@@ -1,25 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const redis = require("redis");
 require("dotenv").config();
+
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const client = redis.createClient(REDIS_PORT);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/query", async (req, res) => {
+const getFromCache = (req, res, next) => {
   const { query } = req.body;
 
-  const axios = require("axios");
+  client.get(query, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(JSON.parse(data));
+    } else {
+      next();
+    }
+  });
+};
+
+app.post("/api/query", getFromCache, async (req, res) => {
+  const { query } = req.body;
 
   const options = {
     method: "GET",
     url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
     params: {
-      query: "pasta",
+      query,
     },
     headers: {
-      "X-RapidAPI-Key": "58653636e0mshbb2255eb1a89c7ep15240ejsn80177795d1e9",
+      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
       "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     },
   };
@@ -27,8 +43,14 @@ app.post("/api/query", async (req, res) => {
   try {
     const response = await axios.request(options);
     console.log(response.data);
+
+    // Set data to Redis
+    client.setex(query, 3600, JSON.stringify(response.data));
+
+    res.send(response.data);
   } catch (error) {
     console.error(error);
+    res.status(500);
   }
 });
 
