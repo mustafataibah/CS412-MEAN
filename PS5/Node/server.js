5e054585f67551bde4166b6c1b4eaca46946ee4f
Mustafa <mustafataibah@gmail.com>
2023-06-27 06:10:37 -0400

    edited html and css and new API

--- PS5/Node/server.js
+++ PS5/Node/server.js
@@ -10,25 +10,30 @@ app.use(express.json());
 app.post("/api/query", async (req, res) => {
   const { query } = req.body;
 
+  const axios = require("axios");
+
   const options = {
     method: "GET",
-    url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup",
+    url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
     params: {
-      term: query,
-      country: "uk",
+      query: "pasta",
     },
     headers: {
-      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-      "X-RapidAPI-Host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+      "X-RapidAPI-Key": "58653636e0mshbb2255eb1a89c7ep15240ejsn80177795d1e9",
+      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     },
   };
 
   try {
     const response = await axios.request(options);
-    console.log(response.data); // Log the response data
-    return res.send(response.data);
+    console.log(response.data);
   } catch (error) {
-    console.error("Error in Axios:", error);
-    return res.status(500).send("Error in Axios: " + error);
+    console.error(error);
   }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
